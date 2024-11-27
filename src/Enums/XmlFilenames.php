06854a440cd6610edd2aaea9cd57bc5836c86d72
mahdiabderraouf <mahdi.abderraouf@outlook.com>
2024-11-27 12:05:43 +0100

    Remove RECHNUNG profile

--- src/Enums/XmlFilenames.php
+++ src/Enums/XmlFilenames.php
@@ -9,9 +9,6 @@ enum XmlFilenames: string
     // only for ZUGFeRD 2.0
     case ZUGFERD = 'zugferd-invoice.xml';
 
-    // Only for RECHNUNG profile
-    case RECHNUNG = 'xrechnung.xml';
-
     public static function values(): array
     {
         return array_map(fn ($case) => $case->value, self::cases());
