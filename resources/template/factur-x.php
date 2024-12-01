e877e2de6f95f852c0b26b5b10d076bb1ca90e21
mahdiabderraouf <mahdi.abderraouf@outlook.com>
2024-12-01 21:49:03 +0100

    phpdoc params

--- resources/template/factur-x.php
+++ resources/template/factur-x.php
@@ -1,13 +1,14 @@
 <?php
 
+use MahdiAbderraouf\FacturX\Models\Buyer;
 use MahdiAbderraouf\FacturX\Models\Invoice;
+use MahdiAbderraouf\FacturX\Models\Seller;
 
 /**
  * @var Invoice $invoice
+ * @var Buyer $buyer
+ * @var Seller $seller
  */
-
-$seller = $invoice->getSeller();
-$buyer = $invoice->getBuyer();
 ?>
 
 <rsm:CrossIndustryInvoice xmlns:qdt="urn:un:unece:uncefact:data:standard:QualifiedDataType:100" xmlns:ram="urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100" xmlns:rsm="urn:un:unece:uncefact:data:standard:CrossIndustryInvoice:100" xmlns:udt="urn:un:unece:uncefact:data:standard:UnqualifiedDataType:100" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
